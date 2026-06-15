9fb0b990723ba7b623001fe6fc64fc4d33833f9b
Bintou <bintouongoiba2018@gmail.com>
2026-06-15 16:45:00 +0000

    deploiement

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const routes = require("./routes");
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3001" }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
